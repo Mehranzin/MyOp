c5b0c12c17b3d2549121890f553a7cc75d153533
Mehran <mehranmesrob@gmail.com>
2025-07-06 01:31:15 -0300

    Agora vai 2.4 - ChatGpt

--- models.py
+++ models.py
@@ -3,13 +3,14 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+password_hash = db.Column(db.Text)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     surname = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.Text)
     age = db.Column(db.Integer)
     nickname = db.Column(db.String(50), unique=True)
 
