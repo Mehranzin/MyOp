4ba087dbd7b5ff0827f88b6c5c848bfbf740cfb0
Mehran <mehranmesrob@gmail.com>
2025-07-06 15:03:24 -0300

    Novo 1.4

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     apelido = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     idade = db.Column(db.Integer, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comments = db.relationship('Comment', backref='author', lazy='dynamic')
