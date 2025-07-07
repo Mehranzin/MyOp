91826d071983b7affaddf01f9b5a28485e6561d8
Mehran <mehranmesrob@gmail.com>
2025-07-07 02:48:33 -0300

    Reset db

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or "dacertoDeus"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgresql://database_gwsy_user:da3yNKwdpJOvSsb7JCtxZOJzFRuG5bAr@dpg-d1lc4g6r433s73dic9lg-a/database_gwsy"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgresql://database_zgax_user:deOzGpWkFnVZqRwakvVP5zjIrQ6McPkj@dpg-d1lltumr433s73dsoki0-a/database_zgax"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
