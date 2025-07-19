f03dc66f174fc771d691321c9b0cc32c06fe9945
Mehran <mehranmesrob@gmail.com>
2025-07-18 23:56:45 -0300

    Correção de apelido na página register

--- forms.py
+++ forms.py
@@ -2,11 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, NumberRange
 from models import User
+from wtforms.validators import Optional
 
 class RegistrationForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired(), Length(max=64)])
     sobrenome = StringField('Sobrenome', validators=[DataRequired(), Length(max=64)])
-    apelido = StringField('Apelido/Anônimo', validators=[DataRequired(), Length(max=64)])
+    apelido = StringField('Apelido/Anônimo', validators=[Optional(), Length(max=64)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
     idade = IntegerField('Idade', validators=[DataRequired(), NumberRange(min=10, max=90)])
     password = PasswordField('Senha', validators=[DataRequired(), Length(min=6)])
@@ -19,7 +20,8 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Email já está em uso.')
 
     def validate_apelido(self, apelido):
-        user = User.query.filter_by(apelido=apelido.data).first()
+        if apelido.data:
+            user = User.query.filter_by(apelido=apelido.data).first()
         if user:
             raise ValidationError('Apelido já está em uso.')
 
