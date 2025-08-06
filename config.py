003a889bc2725d63cbe53632e8cde51997350ab4
Mehran <mehranmesrob@gmail.com>
2025-08-06 20:23:06 -0300

    Novo DataBase Postgers

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or "dacertoDeus"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgresql://database_zgax_user:deOzGpWkFnVZqRwakvVP5zjIrQ6McPkj@dpg-d1lltumr433s73dsoki0-a/database_zgax"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgresql://database_3c3l_user:iUeI609oE6aF4a2WB6PIJm1VpRI3vtLc@dpg-d29u6eidbo4c73ap8rj0-a/database_3c3l"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
